5a8c3f00872937f3a8fd28b9a4bc4b4e7fececb6
jarod <jarod@128.4.145.180>
2019-03-18 14:28:35 -0400

    ePuzzle

--- SudokuBLL/src/main/java/pkgEnum/ePuzzleViolation.java
+++ SudokuBLL/src/main/java/pkgEnum/ePuzzleViolation.java
@@ -1,33 +1,7 @@
 package pkgEnum;
 
-import java.util.Arrays;
-import java.util.ArrayList;
 public enum ePuzzleViolation{
 	ContainsZero,DupCol,DupRegion,DupRow,InvalidValue,MissingZero;
 	
-	private ePuzzleViolation() {
-	}
-	
-	@Override
-	public static ePuzzleViolation valueOf(String name) throws IllegalArgumentException, NullPointerException {
-		for (ePuzzleViolation c : ePuzzleViolation.values()) {
-			
-			if (name == c.toString())
-				return c;
-			else if (name == null)
-				throw new NullPointerException();
-			else
-				throw new IllegalArgumentException();
-			
-		}
-		return ;
-	}
-	@Override
-	public static ePuzzleViolation[] values() {
-		ArrayList<ePuzzleViolation> e = new ArrayList<ePuzzleViolation>();
-		for (ePuzzleViolation c : ePuzzleViolation.values()) {
-			e.add(c);
-		}
-		return e;
-	}
+
 }
